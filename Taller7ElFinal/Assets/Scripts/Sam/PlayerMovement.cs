b66f52f720ef28d7ba9a16ac8fa998626727548d
SamuelElHG <samuel.hernandezg@upb.edu.co>
2023-10-24 22:19:55 -0500

    movement corregido

--- Taller7ElFinal/Assets/Scripts/Sam/PlayerMovement.cs
+++ Taller7ElFinal/Assets/Scripts/Sam/PlayerMovement.cs
@@ -5,44 +5,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float xrot;
-
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
-     private Vector2 mouseMovement;
-     private Vector3 PlayerMovementInput;
-    [SerializeField] private float sensitivity;
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
 
 
-    [Space]
-    [SerializeField] private Transform PlayerCamera;
-    [SerializeField] private Rigidbody rb;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
 
 
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-    // Update is called once per frame
+    public bool canMove = true;
+
+
+    CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
 
-        mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        MovePlayer();
-        MoveCamera(); 
-    }
+        #region Handles Movment
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
 
-    void MovePlayer ()
-    {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * speed;
-        rb.velocity = new Vector3(MoveVector.x,rb.velocity.y, MoveVector.z);
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        #endregion
+
+        #region Handles Jumping
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            moveDirection.y = jumpPower;
         }
-    }
-    void MoveCamera()
-    {
-        xrot -= mouseMovement.y*sensitivity;
-        transform.Rotate(0f, mouseMovement.x * sensitivity, 0f);
-        PlayerCamera.transform.localRotation = Quaternion.Euler(xrot,0,0);
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        #endregion
+
+        #region Handles Rotation
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        #endregion
     }
 }
