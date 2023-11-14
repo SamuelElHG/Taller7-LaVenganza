69a311237c5207a75a214d2f11e584dfe68afe89
SamuelElHG <samuel.hernandezg@upb.edu.co>
2023-11-13 21:14:47 -0500

    gucci?

---
+++ Taller7ElFinal/Assets/Scripts/Sam/Levelfinish.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.SceneManagement;
+
+public class LevelFinish : MonoBehaviour
+{
+    [SerializeField] private int sceneNumber;
+    [SerializeField] private Light theLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        theLight.intensity = 1000f;
+        StartCoroutine(waiChange());
+
+    }
+    IEnumerator waiChange()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
